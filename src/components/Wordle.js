a9802380d30be36f70f783a0123cfe892bfff4d7
Omsurvase24 <omsurvase24@gmail.com>
2024-06-28 23:18:03 +0530

    end game logic

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -9,18 +9,23 @@ export default function Wordle({ solution }) {
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
 
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup])
+        if (isCorrect) {
+            console.log('congrats, you win')
+            window.removeEventListener('keyup', handleKeyup)
+        }
+        if (turn > 5) {
+            console.log('unlucky, out of guesses')
+            window.removeEventListener('keyup', handleKeyup)
+        }
 
-    useEffect(() => {
-        console.log(guesses, turn, isCorrect)
-    }, [guesses, turn, isCorrect])
+        return () => window.removeEventListener('keyup', handleKeyup)
+    }, [handleKeyup, isCorrect, turn])
 
     return (
         <div>
             <div>solution - {solution}</div>
             <div>Current Guess - {currentGuess}</div>
-            <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+            <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
             <Keypad usedKeys={usedKeys} />
         </div>
     )
